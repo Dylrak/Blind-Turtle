cff5366120f58d1ce3b2ed86dafb3cbdd6f6c525
Jan Christiaan Zwier <jczwier@live.nl>
2015-02-22 22:42:18 +0100

    Changed whole UI
    - Added custom action bar with settings and refresh button
    - Added custom tabs to the bottom of the window
    - Added settings window

--- Resources/app.js
+++ Resources/app.js
@@ -1,77 +1,229 @@
-// Creates a tab group
-var tabGroup = Titanium.UI.createTabGroup();
-
-// Creates a window
-var win1 = Titanium.UI.createWindow({  
-    title:'Roosterwijzigingen'
+var win = Titanium.UI.createWindow({
+	backgroundColor: 'white',
+	exitOnClose: true
 });
 
-// Creates a tab for the window
-var tab1 = Titanium.UI.createTab({
-    title:'Roosterwijzigingen',
-    window:win1
+var mainview = Titanium.UI.createView({
+	height:'72.5%',
+	top:'15%'
 });
 
-// Creates a webview for the window
+win.add(mainview);
+
 var webview1 = Titanium.UI.createWebView({
 	url:'http://www3.pj.nl/gym_info_leerlingen/'
 });
 
-// Adds the webview to the window
-win1.add(webview1);
-
-// The process mentioned above is repeated for the other two tabs
-var win2 = Titanium.UI.createWindow({  
-    title:'Mededelingen'
-});
-
-var tab2 = Titanium.UI.createTab({
-    title:'Mededelingen',
-    window:win2
-});
+mainview.add(webview1);
 
 var webview2 = Titanium.UI.createWebView({
 	url:'http://www3.pj.nl/infoschermgymnasium/'
 });
 
-win2.add(webview2);
-
-var win3 = Titanium.UI.createWindow({  
-    title:'Stentor',
-    backgroundColor: '#fff',
-    color: '#000'
+var actionbar = Titanium.UI.createView({
+	backgroundColor:'#dc006d',
+	top:0,
+	height:'15%'
 });
 
-var tab3 = Titanium.UI.createTab({
-    title:'Stentor',
-    window:win3
+var titlelabel = Titanium.UI.createLabel({
+	text:'PJ Info',
+	textAlign:'Titanium.UI.TEXT_ALIGNMENT_CENTER',
+	color:'#fff',
+	font:{fontSize:28}
 });
 
-// Creates a button
-var button = Ti.UI.createButton({
-	title:'Gevleugeld Woord insturen',
-	height:70,
-	width:170,
-	backgroundColor: '#e20177',
-	color: '#fff'
+actionbar.add(titlelabel);
+
+var settingsbutton = Titanium.UI.createImageView({
+	image:'settings.png',
+	height:'60%',
+	left:'5%'
 });
 
-// Adds an event listener to the button that opens an email dialog when the button is clicked/tapped
-button.addEventListener('click',function(e)
-{
-   	var emailDialog = Ti.UI.createEmailDialog();
-	emailDialog.subject = "Gevleugelde Woorden; via Blind Turtle";
-	emailDialog.toRecipients = ['mennohellinga@zoho.com'];	// CHANGE BEFORE RELEASE!
-	emailDialog.open();
+settingsbutton.addEventListener('click',function(){
+	var settingswin = Titanium.UI.createWindow({
+		backgroundColor:'#fff'
+	});
+	
+	var actionbar = Titanium.UI.createView({
+		backgroundColor:'#dc006d',
+		top:0,
+		height:'15%'
+	});
+	
+	var titlelabel = Titanium.UI.createLabel({
+		text:'Instellingen',
+		textAlign:'Titanium.UI.TEXT_ALIGNMENT_CENTER',
+		color:'#fff',
+		font:{fontSize:28}
+	});
+	
+	actionbar.add(titlelabel);
+	
+	settingswin.add(actionbar);
+	settingswin.open();
 });
 
-// Adds the button to the window
-win3.add(button);
+actionbar.add(settingsbutton);
 
-// Adds the tabs to the tab group
-tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
-tabGroup.addTab(tab3);
+var refreshbutton = Titanium.UI.createImageView({
+	image:'refresh.png',
+	height:'60%',
+	right:'5%'
+});
 
-// Opens the tab group
-tabGroup.open();
+refreshbutton.addEventListener('click',function(){
+	webview1.reload();
+	webview2.reload();
+});
+
+actionbar.add(refreshbutton);
+win.add(actionbar);
+
+var tabsview = Titanium.UI.createView({
+	bottom:0,
+	height:'15%',
+	left:0,
+	right:0
+});
+
+win.add(tabsview);
+
+var label1 = Titanium.UI.createLabel({
+	text:'Roosterwijzigingen',
+	textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,
+	top:'15%',
+	bottom:0,
+	left:0,
+	width:'43%',
+	color:'#003c6d',
+	backgroundColor:'#fff'
+});
+
+var label2 = Titanium.UI.createLabel({
+	text:'Mededelingen',
+	textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,
+	top:'15%',
+	bottom:0,
+	left:'44%',
+	width:'34%',
+	color:'#fff',
+	backgroundColor:'#003c6d'
+});
+
+var imageview = Titanium.UI.createView({
+	top:'15%',
+	bottom:0,
+	left:'79%',
+	height:'85%',
+	width:'21%',
+	backgroundColor:'#003c6d'
+});
+
+var image = Titanium.UI.createImageView({
+	image:'stentor.png'
+});
+
+imageview.add(image);
+
+var divisionborder1 = Ti.UI.createView({
+    backgroundColor:'#dc006d',
+    top:'15%',
+    bottom:0,
+    width:'1%',
+    left:'43%',
+    right:'55%'
+});
+
+var divisionborder2 = Ti.UI.createView({
+    backgroundColor:'#dc006d',
+    top:'15%',
+    bottom:0,
+    width:'1%',
+    left:'78%',
+    right:'21%'
+});
+
+tabsview.add(divisionborder1);
+tabsview.add(divisionborder2);
+
+tabsview.add(label1);
+tabsview.add(label2);
+tabsview.add(imageview);
+
+label1.addEventListener('click',function(){
+	label1.color = '#003c6d';
+	label1.backgroundColor = '#fff';
+	
+	label2.color = '#fff';
+	label2.backgroundColor = '#003c6d';
+	
+	imageview.backgroundColor = '#003c6d';
+	
+	mainview.remove(webview2);
+	mainview.add(webview1);
+});
+label2.addEventListener('click',function(){
+	label1.color = '#fff';
+	label1.backgroundColor = '#003c6d';
+	
+	label2.color = '#003c6d';
+	label2.backgroundColor = '#fff';
+	
+	imageview.backgroundColor = '#003c6d';
+	
+	mainview.remove(webview1);
+	mainview.add(webview2);
+});
+imageview.addEventListener('click',function(){
+	label1.color = '#fff';
+	label1.backgroundColor = '#003c6d';
+	
+	label2.color = '#fff';
+	label2.backgroundColor = '#003c6d';
+	
+	imageview.backgroundColor = '#fff';
+	
+	mainview.remove(webview1);
+	mainview.remove(webview2);
+	actionbar.remove(refreshbutton);
+	
+	var stentorview = Titanium.UI.createView({
+    	backgroundColor: '#fff',
+    	height:'72.5%',
+    	top:'15%'
+	});
+	
+	var gwbutton = Ti.UI.createButton({
+		title:'Gevleugelde Woorden insturen',
+		height:70,
+		width:170,
+		backgroundColor: '#dc006d',
+		color: '#fff'
+	});
+	
+	gwbutton.addEventListener('click',function(e)
+	{
+	   	var emailDialog = Ti.UI.createEmailDialog();
+		emailDialog.subject = "Gevleugelde Woorden via PJ Info";
+		emailDialog.toRecipients = ['mennohellinga@zoho.com'];	// CHANGE BEFORE RELEASE!
+		emailDialog.open();
+	});
+	
+	label1.addEventListener('click',function(){
+		actionbar.add(refreshbutton);
+		mainview.remove(stentorview);
+		mainview.add(webview1);
+	});
+	label2.addEventListener('click',function(){
+		actionbar.add(refreshbutton);
+		mainview.remove(stentorview);
+		mainview.add(webview2);
+	});
+	
+	stentorview.add(gwbutton);
+	mainview.add(stentorview);
+});
+
+win.open();
