14f6104d512eae0ff08f95f8f4024d9afa84deca
dylrak <dylrak@hotmail.com>
2015-03-09 19:16:33 +0100

    less is more?

--- Resources/app.js
+++ Resources/app.js
@@ -36,11 +36,7 @@ function loadschedule(){
 	var schedulechanges = Titanium.Filesystem.getFile(Titanium.Filesystem.tempDirectory,'schedulechanges');
 	var dschedule = Titanium.Filesystem.getFile(schedulechanges.nativePath,'schedulechanges.html');
 	var template = Titanium.Filesystem.getFile(Titanium.Filesystem.resourcesDirectory, 'schedulechangestemplate.html');
-	dschedule.createFile();
-	
-	if (dschedule.exists()){
 	dschedule.write(template.read() + tables + '</center></body></html>');
-	}
 }
 
 var win = Titanium.UI.createWindow({
